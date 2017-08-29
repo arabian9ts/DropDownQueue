0297145982f9ffa7de046056eaea7acf46ec92b1
arabian9ts <nok14.tm59@gmail.com>
2017-08-29 14:11:52 +0900

    add: Application extension

---
+++ DropDown/Extension.swift
@@ -0,0 +1,27 @@
+//
+//  Extension.swift
+//  DropDown
+//
+//  Created by arabian9ts on 2017/08/29.
+//
+//  http://qiita.com/The_Shimon/items/80dbf50bc85c527f10f3 writen by The_Shimon
+
+import Foundation
+import UIKit
+
+extension UIApplication {
+    static func frontmost(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return frontmost(controller: navigationController.visibleViewController)
+        }
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return frontmost(controller: selected)
+            }
+        }
+        if let presented = controller?.presentedViewController {
+            return frontmost(controller: presented)
+        }
+        return controller
+    }
+}
