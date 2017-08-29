5987e0ee0dd14f299cff53c46efc8983ea5badd0
arabian9ts <nok14.tm59@gmail.com>
2017-08-29 23:37:17 +0900

    add: new framework DropDown

---
+++ DropDown/DropDown.h
@@ -0,0 +1,19 @@
+//
+//  DropDown.h
+//  DropDown
+//
+//  Created by arabian9ts on 2017/08/29.
+//  Copyright © 2017年 arabian9ts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DropDown.
+FOUNDATION_EXPORT double DropDownVersionNumber;
+
+//! Project version string for DropDown.
+FOUNDATION_EXPORT const unsigned char DropDownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DropDown/PublicHeader.h>
+
+
